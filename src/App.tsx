8e7a0688b4ca972a703dff721dd6ca3aa3591a7d
jimin <moana16@naver.com>
2023-10-15 21:09:59 +0900

    redux 앱 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { RootState } from './reducers';
+import { useSelector ,useDispatch} from 'react-redux/es/exports';
+type Props = {
+  value : any;
+  onIncrement : () => void;
+  onDecrement : () => void;
+}
 
-function App() {
+function App({value, onIncrement, onDecrement} : Props) {
+  const [todoValue, setTodoValue] = useState("");
+  const todos : string[] = useSelector((state:RootState)=>state.todos);
+  const counter = useSelector((state : RootState)=>state.counter);
+  const dispatch = useDispatch();
+
+  const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  }
+  const addTodo = (e : React.FormEvent<HTMLFormElement>) : void => {
+    e.preventDefault();
+    dispatch({type : 'ADD_TODO', text : todoValue});
+    setTodoValue("");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Clicked : {counter} times</p>
+      <button onClick={onIncrement}>+</button>
+      <button onClick={onDecrement}>-</button>
+      <ul>
+        {todos.map((todo, index) => <li key={index}>{todo}</li>)}
+      </ul>
+      <form onSubmit={addTodo}>
+        <input type="text" value={todoValue} onChange={handleChange} />
+        <input type='submit' />  
+      </form>
     </div>
+    
   );
 }
 
